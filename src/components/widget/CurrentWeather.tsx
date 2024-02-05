38d3975126daf86b82c378bec659f1d2d5513777
THEALIFHAKER1 <alifhaker1@gmail.com>
2024-02-05 10:31:07 +0800

    improve look

--- src/components/widget/CurrentWeather.tsx
+++ src/components/widget/CurrentWeather.tsx
@@ -15,9 +15,8 @@ interface CurrentWeatherProps {
 
 export default function CurrentWeather({ data }: CurrentWeatherProps) {
   const initial = new Date()
-
   return (
-    <Card className="relative flex h-fit w-full shrink-0 flex-col justify-between overflow-hidden md:h-[25rem]">
+    <Card className="relative flex h-fit w-full shrink-0 flex-col justify-between overflow-hidden p-10 md:h-[25rem] md:w-[25rem]">
       <div className="absolute " />
       <div>
         <div className="flex justify-between text-lg font-semibold">
@@ -26,26 +25,8 @@ export default function CurrentWeather({ data }: CurrentWeatherProps) {
         </div>
         <div className="text-md mt-2 flex font-bold">
           <span>{data.name}</span>
-          <i>
-            <svg
-              viewBox="0 0 24 24"
-              xmlns="http://www.w3.org/2000/svg"
-              className="ml-0.5 h-4 w-4 fill-none stroke-black dark:stroke-white"
-            >
-              <path
-                d="M7.39993 6.32003L15.8899 3.49003C19.6999 2.22003 21.7699 4.30003 20.5099 8.11003L17.6799 16.6C15.7799 22.31 12.6599 22.31 10.7599 16.6L9.91993 14.08L7.39993 13.24C1.68993 11.34 1.68993 8.23003 7.39993 6.32003Z"
-                strokeWidth="1.5"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              />
-              <path
-                d="M10.1101 13.6501L13.6901 10.0601"
-                strokeWidth="1.5"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              />
-            </svg>
-          </i>
+          <span className="mx-1">,</span>
+          <span>{data.sys.country}</span>
         </div>
       </div>
       <div className="flex justify-center py-7 text-8xl font-bold md:py-10">
@@ -54,7 +35,7 @@ export default function CurrentWeather({ data }: CurrentWeatherProps) {
       <div>
         <IconComponent
           weatherCode={data.weather[0].id}
-          // x={data.sys}
+          x={data.weather[0].icon.slice(-1)}
           className="h-9 w-9"
         />
         <div className="font-semibold">{data.weather[0].main}</div>
